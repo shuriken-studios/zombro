5f5288d1158167ae6cb00c372a867b76fd1046e4
kerrydp8 <50590725+kerrydp8@users.noreply.github.com>
2019-06-04 23:25:16 -0400

    Basic bullet functionality started

--- Zombro/Assets/Scripts/LeePistol.cs
+++ Zombro/Assets/Scripts/LeePistol.cs
@@ -1,41 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class LeePistol : MonoBehaviour
 {
+    public bool pistolActive; //Tells whether or not this weapon is in use.
+    public Rigidbody pistolBulletPrefab;
+    public Transform spawnPoint; //This is where the bullet appears.
 
-    public bool pistolActive; //This is the weapon the player is currently using.
-    public GameObject Bullet; //The actual bullet
-    public Transform spawnPoint; //This is where the bullet will first appear
-    public int bulletSpeed; //The speed of the bullet
-    public float bulletEnd = 3.0f; //The bullet will dissappear after three seconds
-    public bool canShoot = true; //Gun can be fired
-    public float waitBeforeNextShot = 0.25f; //Bullets can be fired at this time interval
-
-    private void Update()
+    void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse0))
+        if (Input.GetMouseButtonDown(0))
         {
-            if (canShoot)
-            {
-                canShoot = false;
-                Shoot();
-                StartCoroutine(ShootingYield());
-            }
+            Rigidbody pistolBulletInstance = Instantiate(pistolBulletPrefab, spawnPoint.position, spawnPoint.rotation) as Rigidbody;
+            pistolBulletInstance.AddForce(spawnPoint.forward * 2000);
+
+            Debug.Log("click");
         }
-    }
-
-    IEnumerator ShootingYield()
-    {
-        yield return new WaitForSeconds(waitBeforeNextShot);
-        canShoot = true;
-    }
-    void Shoot() //This method instantiates the bullet.
-    {
-        var bullet = Instantiate(Bullet, spawnPoint.position, spawnPoint.rotation);
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * bulletSpeed;
-
-        Destroy(bullet, bulletEnd);
     }
 }
