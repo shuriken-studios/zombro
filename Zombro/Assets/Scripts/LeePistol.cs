baab3a6a9394f20efd34cbacc6a3d16123cda49c
kerrydp8 <50590725+kerrydp8@users.noreply.github.com>
2019-06-05 22:51:02 -0400

    Starting to work on pistol ammo

--- Zombro/Assets/Scripts/LeePistol.cs
+++ Zombro/Assets/Scripts/LeePistol.cs
@@ -8,6 +8,7 @@ public class LeePistol : MonoBehaviour
     public PistolBulletController pistolBullet;
     public float bulletSpeed; //Speed of bullet
     public Transform spawnPoint;
+    public float pistolAmmo = 50; //Amount of ammunition
 
     void Start()
     {
@@ -17,17 +18,33 @@ public class LeePistol : MonoBehaviour
 
     void Update()
     {
+
+        if (pistolAmmo >= 300) //Sets a cap of 300 pistol bullets
+        {
+            pistolAmmo = 300;
+        }
+
+        if (pistolAmmo <= 0) //Sets a min of 0 bullets.
+        {
+            pistolAmmo = 0;
+        }
+
         if (LeeController.leeActive) //Lee must be in control for the player to use his gun
         {
             if (pistolActive) //The pistol is the current weapon in use
             {
                 if (firing)
                 {
-                    if (Input.GetMouseButtonDown(0)) //Fires a bullet when the player left-clicks.
+                    if (pistolAmmo > 0)
                     {
-                        PistolBulletController bulletInstance = Instantiate(pistolBullet, spawnPoint.position, spawnPoint.rotation) as PistolBulletController;
-                        bulletInstance.speed = bulletSpeed;
-                        StartCoroutine(Fire());
+                        if (Input.GetMouseButtonDown(0)) //Fires a bullet when the player left-clicks.
+                        {
+                            PistolBulletController bulletInstance = Instantiate(pistolBullet, spawnPoint.position, spawnPoint.rotation) as PistolBulletController;
+                            bulletInstance.speed = bulletSpeed;
+                            pistolAmmo -= 1;
+                            Debug.Log(pistolAmmo);
+                            StartCoroutine(Fire());
+                        }
                     }
                 }
             }
