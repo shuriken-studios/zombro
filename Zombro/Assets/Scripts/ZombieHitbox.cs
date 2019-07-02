3c9cb893a11bd352b9369ca94951e2080f843c9c
kerrydp8 <50590725+kerrydp8@users.noreply.github.com>
2019-07-02 15:19:07 -0400

    Working on health bar

--- Zombro/Assets/Scripts/ZombieHitbox.cs
+++ Zombro/Assets/Scripts/ZombieHitbox.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ZombieHitbox : MonoBehaviour
 {
+    Renderer rend;
     float enemyHealth = 100;
+    //public Text EnemyHealthUI;
+    public GameObject enemyHealthBar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        //EnemyHealthUI.GetComponent<Text>().text = "100";
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (enemyHealth == 20)
+        {
+
+        }
+
         if (enemyHealth == 0) //Once the enemy is out of health, it will dissappear. This code may be modified to leave a corpse behind or something. 
         {
             Destroy(this.transform.parent.gameObject); //Enemy is destroyed. Will dissappear.
+            Destroy(this.enemyHealthBar);
         }
     }
 
