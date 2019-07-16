827437d2b364d63299c8f3815054a643c2c35be5
kerrydp8 <50590725+kerrydp8@users.noreply.github.com>
2019-07-16 17:14:18 -0400

    Changes color now

---
+++ Zombro/Assets/Scripts/ZombieHealthbar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ZombieHealthbar : MonoBehaviour
+{
+    public Image currentHealthbar;
+    public Text ratioText;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        UpdateHealth();
+    }
+
+    // Update is called once per frame
+    void UpdateHealth()
+    {
+        float ratio -= 
+    }
+}
